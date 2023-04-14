05f7144a65fc213ce4ed7ae90df13bce342ddc4e
Enrique Benimeli <ebenimeli@gmail.com>
2023-04-14 20:59:01 +0200

    New data set: words and languages

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 $topic = 'general-es-en';
 
 if ( !isset( $_GET[ 'topic' ] ) ) {
-    $random_number = rand(1, 9);
+    $random_number = rand(1, 10);
     $topic = 'data_' . $random_number;
 } else {
     $topic = $_GET[ 'topic' ];
